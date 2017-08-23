d8c1d9ed7c46a9afd1f005d82359023377fdfa92
Narges17 <narges.yousefnezhad@aalto.fi>
2017-08-23 10:48:56 +0300

    new version of JavaXB generated Java classes

--- O-MI-O-DF-Parser/xsdFiles/omi/MsgType.java
+++ O-MI-O-DF-Parser/xsdFiles/omi/MsgType.java
@@ -12,23 +12,25 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlAnyElement;
+import javax.xml.bind.annotation.XmlMixed;
 import javax.xml.bind.annotation.XmlType;
+import org.w3c.dom.Element;
 
 
 /**
- * List of results.
+ * Payload for the protocol
  * 
- * <p>Java class for ResponseListType complex type.
+ * <p>Java class for MsgType complex type.
  * 
  * <p>The following schema fragment specifies the expected content contained within this class.
  * 
  * <pre>
- * &lt;complexType name="ResponseListType">
+ * &lt;complexType name="MsgType">
  *   &lt;complexContent>
  *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
  *       &lt;sequence>
- *         &lt;element name="result" type="{http://www.opengroup.org/xsd/omi/1.0/}RequestResultType" maxOccurs="unbounded"/>
+ *         &lt;any processContents='lax' namespace='##other' maxOccurs="unbounded" minOccurs="0"/>
  *       &lt;/sequence>
  *     &lt;/restriction>
  *   &lt;/complexContent>
@@ -38,41 +40,44 @@ import javax.xml.bind.annotation.XmlType;
  * 
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "ResponseListType", propOrder = {
-    "result"
+@XmlType(name = "MsgType", propOrder = {
+    "content"
 })
-public class ResponseListType {
+public class MsgType {
 
-    @XmlElement(required = true)
-    protected List<RequestResultType> result;
+    @XmlMixed
+    @XmlAnyElement(lax = true)
+    protected List<Object> content;
 
     /**
-     * Gets the value of the result property.
+     * Payload for the protocol Gets the value of the content property.
      * 
      * <p>
      * This accessor method returns a reference to the live list,
      * not a snapshot. Therefore any modification you make to the
      * returned list will be present inside the JAXB object.
-     * This is why there is not a <CODE>set</CODE> method for the result property.
+     * This is why there is not a <CODE>set</CODE> method for the content property.
      * 
      * <p>
      * For example, to add a new item, do as follows:
      * <pre>
-     *    getResult().add(newItem);
+     *    getContent().add(newItem);
      * </pre>
      * 
      * 
      * <p>
      * Objects of the following type(s) are allowed in the list
-     * {@link RequestResultType }
+     * {@link String }
+     * {@link Element }
+     * {@link Object }
      * 
      * 
      */
-    public List<RequestResultType> getResult() {
-        if (result == null) {
-            result = new ArrayList<RequestResultType>();
+    public List<Object> getContent() {
+        if (content == null) {
+            content = new ArrayList<Object>();
         }
-        return this.result;
+        return this.content;
     }
 
 }
